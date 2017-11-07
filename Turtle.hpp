e212f78fc93cb5df75a975d3bfcd53137501c462
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-07 10:46:52 -0500

    Update Turtle.hpp

--- Turtle.hpp
+++ Turtle.hpp
@@ -1,8 +1,13 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #include <Terminate/terminate.hpp>
 #include <Terminate/sdl/context.hpp>
+#include <cstdlib>
 
-void CheckSDLInit(signed short int& sdlReturnVal) {
-    if (sdlReturnVal < 0) {
+using namespace Term;
+
+void Turtle_Init(Uint32 subsystem_mask) {
+    if (SDL_Init(subsystem_mask) < 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         exit(1);
     }
