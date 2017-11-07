9f0b8dc1f4d6bfcbfbbfe2af68b88307d3586645
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-07 11:14:50 -0500

    Update Turtle.hpp

--- Turtle.hpp
+++ Turtle.hpp
@@ -2,13 +2,31 @@
 #include <SDL/SDL_image.h>
 #include <Terminate/terminate.hpp>
 #include <Terminate/sdl/context.hpp>
-#include <cstdlib>
 
-using namespace Term;
+using namespace Term::SDL;
 
-void Turtle_Init(Uint32 subsystem_mask) {
+void Turtle_Init(Term::SDL::Context term, Term::TTY tty, std::string tileset, SDL_Surface* screen, Uint32 subsystem_mask) {
     if (SDL_Init(subsystem_mask) < 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         exit(1);
+    } else if (SDL_WasInit(SDL_INIT_VIDEO) != 0){
+        SDL_Log("SDL VIDEO NOT INITIALIZED");
+        exit(1);
+    } else {
+        atexit(SDL_Quit);
+        atexit(IMG_Quit);
+        
+        term.Tilemap(tileset);
+        term.Framebuffer().Clear();
+        Term::TTY tty( term.Framebuffer() );
+        if((screen = SDL_SetVideoMode(term.Framebuffer().Width()  * term.TileWidth(), term.Framebuffer().Height() * term.TileHeight(), 32, SDL_SWSURFACE )) == NULL){
+            std::cerr << "Video initialization failed: " << SDL_GetError() << std::endl;
+            exit(1);
+        }
+        term.RenderTarget( screen );
     }
+}
+
+void termClear(){
+    
 }
