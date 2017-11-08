1629e29e5bec35e3c659012f5a5bbed0d0f23224
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-08 12:02:31 -0500

    Update labyrinth.cpp

--- demo/labyrinth.cpp
+++ demo/labyrinth.cpp
@@ -3,10 +3,7 @@
 #include <algorithm>
 #include <sstream>
 #include <vector>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-#include <Terminate/terminate.hpp>
-#include <Terminate/sdl/context.hpp>
+#include "TurtleSDL2.h"
 
 // Possible movement. Each direction has dx,dy and a flag bit
 enum { North=1, East=2, South=4, West=8 };
@@ -40,7 +37,8 @@ RandomColor()
     return Term::Color(
         static_cast<Term::Color::component_t>( rand() % 255 ),
         static_cast<Term::Color::component_t>( rand() % 255 ),
-        static_cast<Term::Color::component_t>( rand() % 255 ) );
+        static_cast<Term::Color::component_t>( rand() % 255 ),
+        static_cast<Term::Color::component_t>1);
     }
 
 
@@ -235,25 +233,43 @@ main( int argc, char* argv[] )
     srand(time(nullptr));
     LabData lab( 31, 31 );
 
-    // Setup SDL related stuff
+    size_t Number_Of_Term_Collems = 17;
+    size_t Number_Of_Term_Rows = 16;
+    
+    // Setup SDL stuff
     SDL_Init( SDL_INIT_VIDEO );
     atexit( SDL_Quit );
     atexit( IMG_Quit );
-
-    size_t hwin = 17;
-    size_t wwin = 16;
-    Term::SDL::Context term( wwin, hwin );
+    
+    /* Term::SDL::Context term( Width-In-Collems, Height-In-Rows ); */
+    Term::SDL::Context term( Number_Of_Term_Collems, Number_Of_Term_Rows );
+    
+    /* Tileset of ASCII characters */
     term.Tilemap( "tileset.png" );
-    SDL_Surface* screen = SDL_SetVideoMode(
-        term.Framebuffer().Width()  * term.TileWidth(),
+    
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurf = NULL;
+    
+    window = SDL_CreateWindow(
+        "Maze", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        term.Framebuffer().Width()  * term.TileWidth(), 
         term.Framebuffer().Height() * term.TileHeight(),
-        32, SDL_SWSURFACE );
-    term.RenderTarget( screen );
+        SDL_WINDOW_SHOWN
+    );
+    
+    screenSurf = SDL_GetWindowSurface( window );
+    
+    /* Take a SDL_Surface to draw to */
+    term.RenderTarget( screenSurf );
+    
     Term::Char clearChar('\0');
     clearChar.PriColor( Term::Color::Black );
     clearChar.SecColor( Term::Color::Black );
     term.Framebuffer().ClearChar( clearChar );
-    SDL_EnableKeyRepeat( 100, 100 ); // Basically the movementspeed of the player.
+    
+    SDL_EnableKeyRepeat( 100, 100 ); // The movementspeed of the player.
 
     Term::TTY tty( term.Framebuffer() );
 
@@ -263,38 +279,43 @@ main( int argc, char* argv[] )
     bool running = true;
     while(running)
         {
-        SDL_Event event;
-        while( SDL_PollEvent(&event) ) switch( event.type )
+            SDL_Event event;
+            while( SDL_PollEvent(&event) )
             {
-            case SDL_QUIT:
-                running = false; break;
-            case SDL_KEYDOWN: switch( event.key.keysym.sym )
+                switch( event.type )
                 {
-                case SDLK_ESCAPE:
-                    running = false; break;
-                case SDLK_UP:
-                    MovePlayer( lab, 0, -1 ); break;
-                case SDLK_DOWN:
-                    MovePlayer( lab, 0, 1 ); break;
-                case SDLK_LEFT:
-                    MovePlayer( lab, -1, 0 ); break;
-                case SDLK_RIGHT:
-                    MovePlayer( lab, 1, 0 ); break;
-                default: break;
-                } break;
-            }
+                    case SDL_QUIT:
+                        running = false; break;
+                    case SDL_KEYDOWN:
+                        switch( event.key.keysym.sym )
+                        {
+                            case SDLK_ESCAPE:
+                                running = false; break;
+                            case SDLK_UP:
+                                MovePlayer( lab, 0, -1 ); break;
+                            case SDLK_DOWN:
+                                MovePlayer( lab, 0, 1 ); break;
+                            case SDLK_LEFT:
+                                MovePlayer( lab, -1, 0 ); break;
+                            case SDLK_RIGHT:
+                                MovePlayer( lab, 1, 0 ); break;
+                            default: break;
+                        }
+                        break;
+                }
+        }
 
         if( lab.win )
-            {
+        {
             std::string winstr( "!!!WIN!!!" );
             int x = rand() % (term.Framebuffer().Width()+winstr.length()) - winstr.length();
             int y = rand() % (term.Framebuffer().Height()+winstr.length()) - winstr.length();
             tty.PriColor( RandomColor() );
             tty.SecColor( RandomColor() );
             tty.Place(x,y).Put( winstr );
-            }
+        }
         else
-            {
+        {
             term.Framebuffer().Clear();
             term.Framebuffer().Copy( lab.symbuf, 0, 1,
                 -(term.Framebuffer().Width()/2) + lab.xplayer,
@@ -305,31 +326,31 @@ main( int argc, char* argv[] )
             tty.SecColor( Term::Color::Black );
             tty.Put( 1 );
             for( auto itoken : lab.tokens )
-                {
+            {
                 tty.Place(
                     (itoken % lab.width) +(term.Framebuffer().Width())/2 - lab.xplayer ,
                     (itoken / lab.width) +(term.Framebuffer().Height()/2) - lab.yplayer +1 );
                 tty.Put( 9 );
-                }
+            }
             tty.Place(0,0).PriColor( Term::Color::Black ).SecColor( Term::Color::White );
             if( lab.door == 0 )
-                {
+            {
                 tty.Put( "Tokens left: " );
                 std::stringstream ss;
                 ss << lab.tokens.size();
                 tty.Put( ss.str() );
-                }
+            }
             else
-                {
+            {
                 tty.Put( "Find the door!" );
                 tty.Place(
                     lab.door % lab.width + term.Framebuffer().Width()/2 - lab.xplayer,
                     lab.door / lab.width + term.Framebuffer().Height()/2 + 1 - lab.yplayer );
                 tty.Put( 239 );
-                }
-            }
+             }
+        }
         term.Print();
-        SDL_Flip(screen);
+        SDL_UpdateWindowSurface(window);
         SDL_Delay(50);
         }
     }
