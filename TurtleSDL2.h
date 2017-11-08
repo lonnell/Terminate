e35a94195e0a07d7f10790abd18e1b88bbdbae01
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-08 10:00:01 -0500

    Update TurtleSDL2.h

--- TurtleSDL2.h
+++ TurtleSDL2.h
@@ -5,7 +5,7 @@
 
 using namespace Turtle::Context = Term::SDL;
 
-inline void Turtle_Init(Term::SDL::Context term, Term::TTY tty, std::string tileset, SDL_Surface* screen, Uint32 subsystem_mask) {
+inline void Turtle_Init(std::string WinName, int posy, int posx, Term::SDL::Context term, Term::TTY tty, std::string tileset, SDL_Window* screen, SDL_Renderer *renderer, Uint32 subsystem_mask, Uint32 WinFlags, Unit32 RenFlags) {
     if (SDL_Init(subsystem_mask) < 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         exit(1);
@@ -19,15 +19,19 @@ inline void Turtle_Init(Term::SDL::Context term, Term::TTY tty, std::string tile
         term.Tilemap(tileset);
         term.Framebuffer().Clear();
         Term::TTY tty( term.Framebuffer() );
-        if((screen = SDL_SetVideoMode(term.Framebuffer().Width()  * term.TileWidth(), term.Framebuffer().Height() * term.TileHeight(), 32, SDL_SWSURFACE )) == NULL){
-            std::cerr << "Video initialization failed: " << SDL_GetError() << std::endl;
+        if((screen = SDL_CreateWindow(WinName.c_str(), posy, posx, term.Framebuffer().Width()  * term.TileWidth(), term.Framebuffer().Height() * term.TileHeight(), WinFlags)) == NULL){
+            std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
+            exit(1);
+        }
+        if((renderer = SDL_CreateRenderer(win, -1, RenFlags)) == NULL){
+            std::cerr << "Could not create renderer " << SDL_GetError() << std::endl;
             exit(1);
         }
         term.RenderTarget( screen );
     }
 }
 
-inline void Turtle_Init_Defualt(Term::SDL::Context term, Term::TTY tty, SDL_Surface* screen){
+inline void Turtle_Init_Defualt(std::string WinName, Term::SDL::Context term, Term::TTY tty, SDL_Window* win, SDL_Renderer *renderer, Uint32 WinFlags){
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         exit(1);
@@ -38,11 +42,15 @@ inline void Turtle_Init_Defualt(Term::SDL::Context term, Term::TTY tty, SDL_Surf
         term.Tilemap("tileset.png");
         term.Framebuffer().Clear();
         Term::TTY tty( term.Framebuffer() );
-        if((screen = SDL_SetVideoMode(term.Framebuffer().Width()  * term.TileWidth(), term.Framebuffer().Height() * term.TileHeight(), 32, SDL_SWSURFACE )) == NULL){
-            std::cerr << "Video initialization failed: " << SDL_GetError() << std::endl;
+        if((win = SDL_CreateWindow(WinName.c_str, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, term.Framebuffer().Width()  * term.TileWidth(), term.Framebuffer().Height() * term.TileHeight(), WinFlags)) == NULL){
+            std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
             exit(1);
         }
-        term.RenderTarget( screen );
+        if((renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED)) == NULL){
+            std::cerr << "Could not create renderer: " << SDL_GetError() << std::endl;
+            exit(1);
+        }
+        term.RenderTarget( win );
         Term::Char clearChar('\0');
         clearChar.PriColor( Term::Color::Black );
         clearChar.SecColor( Term::Color::White );
@@ -54,7 +62,8 @@ inline void termClear(Term::SDL::Context term, Term::TTY tty){
     tty.Place(0, 0);
 }
 
-inline void Term_Flip(SDL_Surface* screen, Term::SDL::Context term){
+inline void Term_Flip(SDL_Renderer* ren, Term::SDL::Context term){
+    SDL_RenderClear(ren);
     term.Print();
-    SDL_Flip(screen);
+    SDL_RenderPresent(ren)
 }
