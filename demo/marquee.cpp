7a149e02d2485da310385ec6c3b76d4328f8266d
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-08 12:15:10 -0500

    Update marquee.cpp

--- demo/marquee.cpp
+++ demo/marquee.cpp
@@ -5,40 +5,56 @@
 #include <Terminate/sdl/context.hpp>
 
 
-Term::Color
-RandomColor()
-    {
+Term::Color RandomColor()
+{
     return Term::Color (
         static_cast<Uint8>(rand()%255),
         static_cast<Uint8>(rand()%255),
-        static_cast<Uint8>(rand()%255) );
-    }
+        static_cast<Uint8>(rand()%255),
+        static_cast<Uint8>1
+    );
+}
 
 
-void
-RandomColors( Term::String& str )
-    {
+void RandomColors( Term::String& str )
+{
     for( Term::Char& ch : str )
+    {
         ch.PriColor( Term::Color::Black ).SecColor( RandomColor() );
     }
+}
 
 
-int
-main( int argc, char* argv[] )
-    {
+int main( int argc, char* argv[] )
+{
     // Setup SDL and our terminal buffer.
     SDL_Init( SDL_INIT_VIDEO );
 
     atexit( SDL_Quit );
     atexit( IMG_Quit );
+    
+    /* Term::SDL::Context term( Width-In-Collems, Height-In-Rows ); */
     Term::SDL::Context term( 20, 3 );
+    
+    /* Tileset of ASCII characters */
     term.Tilemap( "tileset.png" );
-
-    SDL_Surface* screen = SDL_SetVideoMode(
-        term.Framebuffer().Width()  * term.TileWidth(),
+    
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurf = NULL;
+    
+    window = SDL_CreateWindow(
+        "Marquee", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        term.Framebuffer().Width()  * term.TileWidth(), 
         term.Framebuffer().Height() * term.TileHeight(),
-        32, SDL_SWSURFACE );
-    term.RenderTarget(screen);
+        SDL_WINDOW_SHOWN
+    );
+    
+    screenSurf = SDL_GetWindowSurface( window );
+    
+    /* Take a SDL_Surface to draw to */
+    term.RenderTarget( screenSurf );
 
     // Prepare the marquee!
     Term::String bannerText = Term::MakeString( "Terminate Terminal emulator running SDL" );
@@ -49,27 +65,34 @@ main( int argc, char* argv[] )
     int offset = term.Framebuffer().Width();
     bool running=true;
     while(running)
-        {
+    {
         SDL_Event event;
         while( SDL_PollEvent(&event) ) switch( event.type )
-            {
+        {
             case SDL_QUIT:
             case SDL_KEYDOWN:
                 running = false; break;
-            }
+        }
 
         // Print some utterly unreadable randomly colored text.
         tty.Place( 0, 0 );
         RandomColors(bannerText);
         tty.Put( bannerText );
         term.Framebuffer().Clear();
-        term.Framebuffer().Copy( banner, offset, 1,
-            0,0, banner.Width(), banner.Height() );
+        term.Framebuffer().Copy( 
+            banner, 
+            offset, 
+            1,
+            0,
+            0, 
+            banner.Width(), 
+            banner.Height()
+        );
         term.Print();
-        SDL_Flip(screen);
+        SDL_UpdateWindowSurface(window);
         SDL_Delay(50);
         // Move text and wrap around screen.
         if( --offset < -(int)banner.Width() )
             offset = term.Framebuffer().Width();
-        }
-    }
+     }
+}
