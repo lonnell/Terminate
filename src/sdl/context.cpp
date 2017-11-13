2b7198a66e81347bece72e54402b63043350805f
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-13 11:49:02 -0500

    Update context.cpp

--- src/sdl/context.cpp
+++ src/sdl/context.cpp
@@ -25,11 +25,14 @@ namespace SDL
         {
         }
 
-    /*
      Context::~Context()
      {
         if( tilemap != nullptr ){
-            SDL_DestroyTexture(tilemap);
+            SDL_FreeSurface(tilemap);
+            tilemap = NULL;
+        }
+        if( drawSurf != nullptr ){
+            SDL_DestroyTexture(drawSurf);
             tilemap = NULL;
         }
         if(trenderer != nullptr){
@@ -41,60 +44,29 @@ namespace SDL
             twindow = NULL;
         }
      }
-     */
-    Context::~Context()
-        {
-        if( tilemap != nullptr )
-            SDL_FreeSurface(tilemap);
-        }
 
-
-    /*
-    void Context::RenderTarget( SDL_Window* win ){
-        trenderer = SDL_CreateRenderer( win, -1, SDL_RENDERER_ACCELERATED );
+    //void Context::RenderTarget
+    void Context::WinInit(std::string winName, int widthx, int heighty)
+    {
+        twindow = SDL_CreateWindow(
+            winName.c_str(), 
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+            widthx, 
+            heighty,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+            );
+        trenderer = SDL_CreateRenderer( twindow, -1, SDL_RENDERER_ACCELERATED);
         if (trenderer == nullptr){
             throw std::runtime_error( "Renderer could not be created! SDL Error: "+ IMG_GetError());
         } else {
+            SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+            SDL_RenderSetLogicalSize(trenderer, widthx, heighty);
             SDL_SetRenderDrawColor( trenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+            SDL_RenderClear( trenderer );
         }
     }
-    */
-    void
-    Context::RenderTarget( SDL_Surface* surf )
-        {
-        drawSurf = surf;
-        }
 
 
-    /*
-        void Context::Tilemap( std::string path )
-        {
-            //Load image at specified path
-            SDL_Surface* newTilemap = IMG_Load( path.c_str() );
-            if( newTilemap == NULL ){
-                throw std::runtime_error( "Error opening file "+path.c_str()+"! SDL_image Error: " + IMG_GetError());
-            }
-            else
-            {
-                if( newTilemap->format->BitsPerPixel != 8 ){
-                    throw std::runtime_error( "Image must be color indexed: "+path );
-                }
-                if( tilemap != nullptr ){
-                    SDL_DestroyTexture(tilemap);
-                    //tilemap = NULL;
-                }
-                //Create texture from surface pixels
-                tilemap = SDL_CreateTextureFromSurface(trenderer, newTilemap );
-                if( tilemap == NULL ){
-                    throw std::runtime_error( "Unable to create texture from "+path.c_str()+"! SDL_image Error: " + IMG_GetError());
-                }
-                twidth = newTilemap->w / 16;
-                theight = newTilemap->h / 16;
-                SDL_FreeSurface( newTilemap );
-            }
-        }
-        
-    */
     void
     Context::Tilemap( std::string path )
         {
@@ -109,14 +81,9 @@ namespace SDL
         tilemap = newTilemap;
         twidth = tilemap->w / 16;
         theight = tilemap->h / 16;
+        SDL_FreeSurface(newTilemap);
         }
 
-    /*
-    SDL_Texture* Context::Tilemap()
-    {
-        return tilemap;
-    }
-    */
     SDL_Surface*
     Context::Tilemap()
         {
@@ -137,37 +104,16 @@ namespace SDL
         return theight;
         }
 
-    /*
     void Context::Print( Char ch, size_t x, size_t y ) const
     {
         SDL_Color colors[2] = {
                 toSDLColor( ch.PriColor() ),
                 toSDLColor( ch.SecColor() ) };
-        tilemaptex = SDL_CreateTextureFromSurface(trenderer, newTilemap );
-        // Convert the ASCII value to tilemap coordinates.
-        Sint16 tilex = (ch.ASCII() % 16) * TileWidth();
-        Sint16 tiley = (ch.ASCII() / 16) * TileHeight();
-        SDL_Rect tile = {
-            tilex, tiley,
-            static_cast<Uint16>(TileWidth()),
-            static_cast<Uint16>(TileHeight())};
-        SDL_Rect dst = {
-            static_cast<Sint16>(x * TileWidth()),
-            static_cast<Sint16>(y * TileHeight()),
-            0,0 };
-        SDL_RenderCopy(trenderer, tilemaptex, tile, dst);
-    }
-    */
-    void
-    Context::Print( Char ch, size_t x, size_t y ) const
-        {
-        SDL_Color colors[2] = {
-            toSDLColor( ch.PriColor() ),
-            toSDLColor( ch.SecColor() ) };
         if( SDL_SetColors( tilemap, colors, 0, 2 ) == 0 )
             throw std::runtime_error(
                 "Tilemap missing palette." );
-
+                
+        drawSurf = SDL_CreateTextureFromSurface(trenderer, tilemap );
         // Convert the ASCII value to tilemap coordinates.
         Sint16 tilex = (ch.ASCII() % 16) * TileWidth();
         Sint16 tiley = (ch.ASCII() / 16) * TileHeight();
@@ -179,9 +125,8 @@ namespace SDL
             static_cast<Sint16>(x * TileWidth()),
             static_cast<Sint16>(y * TileHeight()),
             0,0 };
-
-        SDL_BlitSurface( tilemap, &tile, drawSurf, &dst );
-        }
+        SDL_RenderCopy(trenderer, drawSurf, tile, dst);
+    }
 
 
     void
